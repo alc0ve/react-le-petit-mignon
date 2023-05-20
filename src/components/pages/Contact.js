a43ad6f9e944901e14a2088c536ef186df6641a6
Christina Hall <cmcclai26@gmail.com>
2023-05-20 18:37:50 -0400

    contact page and email validator added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../utils/helper';
 
 export default function Contact() {
+  const [email, setEmail] = useState('');
+  const [userName, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'Name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+   
+      return;
+    }
+
+    setName('');
+    setMessage('');
+    setEmail('');
+  };
+
   return (
-    <div>
+    <div className='container'>
       <h1>Contact Me!</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <br></br>
+      <form>
+        <div className='mb-3'>
+        <label htmlFor="InputName" className="form-label">Name</label>
+        <input
+          value={userName}
+          name="Name"
+          onChange={handleInputChange}
+          type="text"
+          className="form-control" 
+          placeholder="name"
+        />
+        </div>
+        <div className='mb-3'>
+        <label htmlFor="InputEmail" className="form-label">Email address</label>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          className="form-control" 
+          placeholder="name@example.com"
+        />
+        </div>
+        <div className='mb-3'>
+        <label htmlFor="InputMessage" className="form-label">What would you like to contact me about?</label>
+        <textarea
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          className="form-control" 
+          placeholder="message"
+          rows={"3"}
+        />
+        </div>
+        <button className="btn btn-primary" type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
