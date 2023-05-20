9a3ba8faccaa5f17317d68a3fc23c22cf5069863
Christina Hall <cmcclai26@gmail.com>
2023-05-20 15:40:33 -0400

    tailwind now functioning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './input.css';
 import PortfolioContainer from "./components/PortfolioContainer";
 
 const App = () => <PortfolioContainer />;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-red-300">
       <h1>Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
