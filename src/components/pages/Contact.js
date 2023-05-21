0512d01868fe2e4f5bd57e379ed6c4da7de6d444
Christina Hall <cmcclai26@gmail.com>
2023-05-21 16:29:36 -0400

    formspree npm added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { validateEmail } from '../utils/helper';
+import { ValidationError, useForm } from '@formspree/react';
+// import { validateEmail } from '../utils/helper';
 import background from '../../assets/img/hero-pattern.jpg';
 
 export default function Contact() {
   const [email, setEmail] = useState('');
   const [userName, setName] = useState('');
   const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage] = useState('');
+  const [state, handleSubmit] = useForm("mbjeejob");
 
   const handleInputChange = (e) => {
 
@@ -21,21 +23,22 @@ export default function Contact() {
     } else {
       setMessage(inputValue);
     }
+
   };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
 
-    if (!validateEmail(email)) {
-      setErrorMessage('Email is invalid');
+  //   if (!validateEmail(email)) {
+  //     setErrorMessage('Email is invalid');
    
-      return;
-    }
+  //     return;
+  //   }
 
-    setName('');
-    setMessage('');
-    setEmail('');
-  };
+  //   setName('');
+  //   setMessage('');
+  //   setEmail('');
+  // };
 
   return (
   <div style={{
@@ -48,7 +51,7 @@ export default function Contact() {
     <div className='max-w-[1040px] m-auto md:pl-20 p-3 py-16'>
       <div className='text-2xl text-black font-semibold my-3'>Contact Me!</div>
       <br></br>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <div className='flex flex-col py-4 gap-2'>
         <label htmlFor="InputName" className="text-lg text-black font-semibold">Name</label>
         <input
@@ -59,7 +62,9 @@ export default function Contact() {
           className="rounded-md p-3 outline outline-offset-2 outline-cyan-500 text-base max-w-prose" 
           placeholder="Name"
         />
+        <ValidationError field="name" prefix="Name" errors={state.errors} />
         </div>
+
         <div className='flex flex-col py-4 gap-2'>
         <label htmlFor="InputEmail" className="text-lg text-black font-semibold">Email address</label>
         <input
@@ -70,7 +75,9 @@ export default function Contact() {
           className="rounded-md p-3 outline outline-offset-2 outline-cyan-500 text-base max-w-prose" 
           placeholder="name@example.com"
         />
+        <ValidationError field="email" prefix="Email" errors={state.errors} />
         </div>
+
         <div className='flex flex-col py-4 gap-2'>
         <label htmlFor="InputMessage" className="text-lg text-black font-semibold">Message</label>
         <textarea
@@ -81,12 +88,13 @@ export default function Contact() {
           className="rounded-md p-3 outline outline-offset-2 outline-cyan-500 text-base max-w-prose" 
           placeholder="What would you like to contact me about?"
         />
+        <ValidationError prefix="Message" field="message" errors={state.errors} />
         </div>
-        <button className="px-5 py-2 text-md text-cyan-500 font-semibold rounded-full border border-cyan-400 hover:text-white hover:bg-cyan-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2" type="button" onClick={handleFormSubmit}>Submit</button>
+        <button className="px-5 py-2 text-md text-cyan-500 font-semibold rounded-full border border-cyan-400 hover:text-white hover:bg-cyan-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2" type="submit" disabled={state.submitting}>Submit</button>
       </form>
       {errorMessage && (
         <div>
-          <p className="error-text">{errorMessage}</p>
+          <p className="error-text">{state.errors}</p>
         </div>
       )}
     </div>
