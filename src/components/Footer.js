53821bad04ce96996ff01c365d168667c18897e9
Christina Hall <cmcclai26@gmail.com>
2023-05-21 15:43:01 -0400

    footer displaying in row

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,33 +4,31 @@ import { SiLeetcode } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <div className="bg-gradient-to-r from-sky-500 via-cyan-400 to-neutral-300 flex space-x-8 text-white">
+    <div className="bg-gradient-to-r from-sky-400 via-cyan-300 to-neutral-100 flex space-x-8 text-white">
       <footer className="flex justify-between">
-        <div className="order-last">
 
-          <ul>
-            <a href="mailto:cmcclai26@gmail.com" title="Email"
+          <ul className="flex flex-row justify-between">
+            <li><a href="mailto:cmcclai26@gmail.com" title="Email"
               target="_blank" rel="noopener noreferrer">
               <FaEnvelope className="m-3" size={42} />
-            </a>
+            </a></li>
 
-            <a href="https://github.com/alc0ve" title="GitHub Profile"
+            <li><a href="https://github.com/alc0ve" title="GitHub Profile"
               target="_blank" rel="noopener noreferrer">
               <FaGithub className="m-3" size={42} />
-            </a>
+            </a></li>
 
-            <a href="https://www.linkedin.com/in/christina-h-05361b25b/" title="LinkedIn Profile"
+            <li><a href="https://www.linkedin.com/in/christina-h-05361b25b/" title="LinkedIn Profile"
               target="_blank" rel="noopener noreferrer">
               <FaLinkedin className="m-3" size={42} />
-            </a>
+            </a></li>
 
-            <a href="https://leetcode.com/cmcclai26/" title="LeetCode Profile"
+            <li><a href="https://leetcode.com/cmcclai26/" title="LeetCode Profile"
               target="_blank" rel="noopener noreferrer">
               <SiLeetcode className="m-3" size={42} />
-            </a>
+            </a></li>
 
           </ul>
-        </div>
       </footer>
     </div >
   );
