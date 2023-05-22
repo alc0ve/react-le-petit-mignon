8ced0bf34757cfb309bc22fd49d682e5107f1ce2
Christina Hall <cmcclai26@gmail.com>
2023-05-22 17:21:08 -0400

    flex wrapping for project cards properly

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import background from '../../assets/img/hero-pattern.jpg';
 import hoppyTrails from '../../assets/img/hoppy-trails.png';
 import ideationApp from '../../assets/img/ideation-app-gen.png';
+import noteTaker from '../../assets/img/note-taker-express.png';
 
 export default function Portfolio() {
   return (
@@ -12,14 +13,13 @@ export default function Portfolio() {
       backgroundPosition: 'center',
     }}className='h-screen'>
 
-    <div className="max-w-[1040px] m-auto md:pl-20 p-3 py-16">
-      <div className="text-2xl text-black font-semibold my-3">Portfolio</div>
+<div className="max-w-[1040px] m-auto md:pl-20 p-3 py-16">
+  <div className="text-2xl text-black font-semibold my-3">Portfolio</div>
 
    <div className="flex flex-wrap">
   
         {/* Brewery Finder */}
-        {/* <div className="col-xs-8 col-sm-6 col-md-4"> */}
-          <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm: flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
+          <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
             <h4 className="text-xl text-black font-semibold my-2">Hoppy Trails</h4>
               <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + hoppyTrails} alt="Hoppy Trails"></img>
             <div className="items-center px-3 space-y-4">
@@ -40,14 +40,12 @@ export default function Portfolio() {
               </ul>
             </div>
           </div>
-        {/* </div> */}
 
-        {/* Brewery Finder */}
-        {/* <div className="col-xs-12 col-sm-6 col-md-4"> */}
-          <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm: flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
+        {/* Ideation: App Generator */}
+          <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
             <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
               <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                      alt="Hoppy Trails"></img>
+                      alt="Ideation"></img>
             <div className="items-center px-8">
                   <p className="text-sm">
                   This application will generate an app idea for anyone looking to create an app.
@@ -66,7 +64,30 @@ export default function Portfolio() {
               </ul>
             </div>
           </div>
-        {/* </div> */}
+
+        {/* Note Taker Express.js */}
+          <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
+            <h4 className="text-xl text-black font-semibold my-3">Note Taker</h4>
+              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + noteTaker}
+                      alt="Note Taker"></img>
+            <div className="items-center px-8">
+                  <p className="text-sm">
+                  This application is used to write and save notes.
+                  </p>
+              <ul className="list-outside flex flex-col items-center space-y-2">
+                <li className="flex">
+                  <a href="https://damp-crag-05718.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                  target="_blank" rel="noopener noreferrer">
+                    Live Site</a>
+                </li>
+                <li className="flex">
+                  <a href="https://github.com/alc0ve/note-taker" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                  target="_blank" rel="noopener noreferrer">
+                    GitHub</a>
+                </li>
+              </ul>
+            </div>
+          </div>
 
         </div>
 
