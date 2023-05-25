2511013536164665dddf052e154104b606083571
Christina Hall <cmcclai26@gmail.com>
2023-05-24 22:12:43 -0400

    fixed up project cards need one more

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="text-2xl text-black font-semibold my-3">Christina Hall</div>
       <div className="py-8 px-8 max-w-[1040px] m-auto space-y-2 flex sm:items-center sm:space-y-0 sm:space-x-6">
         <p className="leading-loose">
-          I am a Developer.
+          Insert something cool here.
         </p>
       </div>
     </div>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,6 +3,8 @@ import React from 'react';
 import hoppyTrails from '../../assets/img/hoppy-trails.png';
 import ideationApp from '../../assets/img/ideation-app-gen.png';
 import noteTaker from '../../assets/img/note-taker-express.png';
+import passwordGen from '../../assets/img/password-generator.png';
+import weatherApp from '../../assets/img/weatherapp.png';
 import Footer from "../Footer"
 
 export default function Portfolio() {
@@ -21,7 +23,7 @@ export default function Portfolio() {
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             {/* card */}
             <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-2">Hoppy Trails</h4>
+              <h4 className="text-xl text-black font-semibold my-3">Hoppy Trails</h4>
               <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + hoppyTrails} alt="Hoppy Trails"></img>
               <div className="items-center px-1 space-y-4">
                 <p className="text-sm">
@@ -98,49 +100,26 @@ export default function Portfolio() {
               </div>
             </div>
           </div>
+
+          {/* Password Generator */}
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             {/* card */}
             <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
-              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                alt="Ideation"></img>
+              <h4 className="text-xl text-black font-semibold my-3">Password Generator</h4>
+              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + passwordGen}
+                alt="Password Generator"></img>
               <div className="items-center px-1">
                 <p className="text-sm">
-                  This application will generate an app idea for anyone looking to create an app.
+                  This application generates random passwords between 8-128 characters.
                 </p>
                 <ul className="list-outside flex flex-col items-center space-y-2">
                   <li className="flex">
-                    <a href="https://ideation-app-generator.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                    <a href="https://alc0ve.github.io/secure-zombie/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
                       target="_blank" rel="noopener noreferrer">
                       Live Site</a>
                   </li>
                   <li className="flex">
-                    <a href="https://github.com/alc0ve/automatic-fortnight-debuggers" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      GitHub</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            {/* card */}
-            <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
-              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                alt="Ideation"></img>
-              <div className="items-center px-1">
-                <p className="text-sm">
-                  This application will generate an app idea for anyone looking to create an app.
-                </p>
-                <ul className="list-outside flex flex-col items-center space-y-2">
-                  <li className="flex">
-                    <a href="https://ideation-app-generator.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      Live Site</a>
-                  </li>
-                  <li className="flex">
-                    <a href="https://github.com/alc0ve/automatic-fortnight-debuggers" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                    <a href="https://github.com/alc0ve/secure-zombie" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
                       target="_blank" rel="noopener noreferrer">
                       GitHub</a>
                   </li>
@@ -149,6 +128,34 @@ export default function Portfolio() {
             </div>
           </div>
 
+          {/* Weather App */}
+          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+            {/* card */}
+            <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
+              <h4 className="text-xl text-black font-semibold my-3">Weather App</h4>
+              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + weatherApp}
+                alt="Weather App"></img>
+              <div className="items-center px-1">
+                <p className="text-sm">
+                  This application will pull weather data for any city entered in the search bar.
+                </p>
+                <ul className="list-outside flex flex-col items-center space-y-2">
+                  <li className="flex">
+                    <a href="https://alc0ve.github.io/weather-app/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                      target="_blank" rel="noopener noreferrer">
+                      Live Site</a>
+                  </li>
+                  <li className="flex">
+                    <a href="https://github.com/alc0ve/weather-app" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                      target="_blank" rel="noopener noreferrer">
+                      GitHub</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          {/*  */}
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             {/* card */}
             <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
@@ -174,111 +181,6 @@ export default function Portfolio() {
               </div>
             </div>
           </div>
-
-          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            {/* card */}
-            <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
-              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                alt="Ideation"></img>
-              <div className="items-center px-1">
-                <p className="text-sm">
-                  This application will generate an app idea for anyone looking to create an app.
-                </p>
-                <ul className="list-outside flex flex-col items-center space-y-2">
-                  <li className="flex">
-                    <a href="https://ideation-app-generator.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      Live Site</a>
-                  </li>
-                  <li className="flex">
-                    <a href="https://github.com/alc0ve/automatic-fortnight-debuggers" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      GitHub</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            {/* card */}
-            <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
-              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                alt="Ideation"></img>
-              <div className="items-center px-1">
-                <p className="text-sm">
-                  This application will generate an app idea for anyone looking to create an app.
-                </p>
-                <ul className="list-outside flex flex-col items-center space-y-2">
-                  <li className="flex">
-                    <a href="https://ideation-app-generator.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      Live Site</a>
-                  </li>
-                  <li className="flex">
-                    <a href="https://github.com/alc0ve/automatic-fortnight-debuggers" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      GitHub</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            {/* card */}
-            <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
-              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                alt="Ideation"></img>
-              <div className="items-center px-1">
-                <p className="text-sm">
-                  This application will generate an app idea for anyone looking to create an app.
-                </p>
-                <ul className="list-outside flex flex-col items-center space-y-2">
-                  <li className="flex">
-                    <a href="https://ideation-app-generator.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      Live Site</a>
-                  </li>
-                  <li className="flex">
-                    <a href="https://github.com/alc0ve/automatic-fortnight-debuggers" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      GitHub</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            {/* card */}
-            <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
-              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                alt="Ideation"></img>
-              <div className="items-center px-1">
-                <p className="text-sm">
-                  This application will generate an app idea for anyone looking to create an app.
-                </p>
-                <ul className="list-outside flex flex-col items-center space-y-2">
-                  <li className="flex">
-                    <a href="https://ideation-app-generator.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      Live Site</a>
-                  </li>
-                  <li className="flex">
-                    <a href="https://github.com/alc0ve/automatic-fortnight-debuggers" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
-                      target="_blank" rel="noopener noreferrer">
-                      GitHub</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
 
 
         </div>
