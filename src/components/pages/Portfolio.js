a8d1eeef35367eee5ebdd43be8ae6fad1b14dd76
Christina Hall <cmcclai26@gmail.com>
2023-05-28 11:57:52 -0400

    added another project card

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,6 +5,7 @@ import ideationApp from '../../assets/img/ideation-app-gen.png';
 import noteTaker from '../../assets/img/note-taker-express.png';
 import passwordGen from '../../assets/img/password-generator.png';
 import weatherApp from '../../assets/img/weatherapp.png';
+import dailyPlanner from '../../assets/img/daily-planner.png';
 import Footer from "../Footer"
 
 export default function Portfolio() {
@@ -153,25 +154,25 @@ export default function Portfolio() {
             </div>
           </div>
 
-          {/*  */}
+          {/* Work Day Scheduler */}
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             {/* card */}
             <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg sm:py-2 sm:flex sm:flex-col sm:items-center sm:space-y-4 sm:space-x-4">
-              <h4 className="text-xl text-black font-semibold my-3">Ideation</h4>
-              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + ideationApp}
-                alt="Ideation"></img>
+              <h4 className="text-xl text-black font-semibold my-3">Work Day Scheduler</h4>
+              <img className="block mx-auto h-auto rounded-xl border-2 border-slate-300 p-2" src={process.env.PUBLIC_URL + dailyPlanner}
+                alt="Daily Planner"></img>
               <div className="items-center px-1">
                 <p className="text-sm">
-                  This application will generate an app idea for anyone looking to create an app.
+                A 9-5 planner used to jot down important tasks during those hours of the day.
                 </p>
                 <ul className="list-outside flex flex-col items-center space-y-2">
                   <li className="flex">
-                    <a href="https://ideation-app-generator.herokuapp.com/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                    <a href="https://alc0ve.github.io/daily-planner/" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
                       target="_blank" rel="noopener noreferrer">
                       Live Site</a>
                   </li>
                   <li className="flex">
-                    <a href="https://github.com/alc0ve/automatic-fortnight-debuggers" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
+                    <a href="https://github.com/alc0ve/daily-planner" className="px-3 py-2 text text-cyan-500 font-semibold hover:text-white hover:bg-cyan-500 hover:border-transparent hover:rounded-lg focus:outline-none focus:rin focus:ring-cyan-600 focus:ring-offset-2"
                       target="_blank" rel="noopener noreferrer">
                       GitHub</a>
                   </li>
