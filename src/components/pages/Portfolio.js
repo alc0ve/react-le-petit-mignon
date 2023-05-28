d791f6a5fe32f3c6fdcc7047fae71c3841d276d4
Christina Hall <cmcclai26@gmail.com>
2023-05-28 13:56:10 -0400

    changed import to variables for images and download resume working

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import background from '../../assets/img/hero-pattern.jpg';
-import hoppyTrails from '/assets/img/hoppy-trails.png';
-import ideationApp from '/assets/img/ideation-app-gen.png';
-import noteTaker from '/assets/img/note-taker-express.png';
-import passwordGen from '/assets/img/password-generator.png';
-import weatherApp from '/assets/img/weatherapp.png';
-import dailyPlanner from '/assets/img/daily-planner.png';
 import Footer from "../Footer"
+
+let hoppyTrails = '/assets/img/hoppy-trails.png';
+let ideationApp = '/assets/img/ideation-app-gen.png';
+let noteTaker = '/assets/img/note-taker-express.png';
+let passwordGen = '/assets/img/password-generator.png';
+let weatherApp = '/assets/img/weatherapp.png';
+let dailyPlanner = '/assets/img/daily-planner.png';
 
 export default function Portfolio() {
   return (
